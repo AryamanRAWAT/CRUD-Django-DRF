a149242791ae935da25011e5d9baef90891070b7
AryamanRAWAT <arawat.kol@gmail.com>
2024-01-30 21:31:36 +0530

    drf applied

--- DProject/settings.py
+++ DProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-63aia(w6+#7@$w#dg=%pnxka&&r&b43g)zz_bb8@h4n%0bi@$q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['aryamanrawat2000.pythonanywhere.com','*']
 
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'DProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dp_assignment',
+		'USER': 'AR',
+        'PASSWORD': 'Admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- api/serializers.py
+++ api/serializers.py
@@ -4,14 +4,7 @@ from rest_framework import serializers
 # from 'api' app
 from .models import user_details
 
-class User_detailsSerializer(serializers.Serializer):
-	id = serializers.IntegerField() 
-	first_name = serializers.CharField(max_length=50)
-	last_name = serializers.CharField(max_length=50)
-	company_name = serializers.CharField(max_length=70)
-	age = serializers.IntegerField()
-	city = serializers.CharField(max_length=50)
-	state = serializers.CharField(max_length=50)
-	zip = serializers.IntegerField()
-	email = serializers.CharField(max_length=50)
-	web = serializers.CharField(max_length=50)
+class User_detailsSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = user_details
+		fields = '__all__'
